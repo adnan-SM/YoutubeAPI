90f9195afd83c163b0076315fa8aaaf6c393179a
Adnan <adnan.coolboy@gmail.com>
2014-02-03 11:14:36 +0530

    Initial Commit

---
+++ src/com/example/youtubeapidemo/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.youtubeapidemo;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener{
+
+	static private final String DEVELOPER_KEY = "AIzaSyCb8Ub2Kuk4RaBI0AyvfUHM63id5_EkHJI";
+    static private final String VIDEO = "P9mybTArlsk";//"830PhCOHeB0";
+   
+    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		YouTubePlayerView youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        youTubeView.initialize(DEVELOPER_KEY, this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void onInitializationFailure(Provider arg0,
+			YouTubeInitializationResult error) {
+		// TODO Auto-generated method stub
+		  Toast.makeText(this, "Oh no! "+error.toString(),
+				  Toast.LENGTH_LONG).show();
+	}
+
+	@Override
+	public void onInitializationSuccess(Provider arg0, YouTubePlayer player,
+			boolean arg2) {
+		// TODO Auto-generated method stub
+		 Log.d("Success", "Came in success");
+		 player.loadVideo(VIDEO);
+	}
+
+}
